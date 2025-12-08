c77f8c4e3ade53e8d4f122c54e007d28f19fd5fb
biswasritwick <ritwick.biswas527@gmail.com>
2025-12-08 20:58:49 +0530

    update

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -33,7 +33,10 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
         <Link href="/">
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          {/* <Logo loading="eager" priority="high" className="invert dark:invert-0" /> */}
+          <div className="text-3xl font-bold text-black mousegrow" id="hero">
+            &lt;RITWICK/&gt;
+          </div>
         </Link>
         <HeaderNav data={data} />
       </div>
